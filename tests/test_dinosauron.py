3cb3dceba47a33d9e6d388ae3dc159cd3ca55d9d
Mike Allred <mikejallred@gmail.com>
2017-12-13 16:13:27 -0500

    Test, travis.

--- tests/test_dinosauron.py
+++ tests/test_dinosauron.py
@@ -4,4 +4,10 @@ from unittest.mock import create_autospec
 
 class TestDinoNmap(unittest.TestCase):
     def setUp(self):
-        self.dn = dino_nmap.DinoNmap()
+        self.d = dinosauron.Dinosauron()
+
+    def test_dig_mdns(self):
+        mock = create_autospec(self.d.dig_mdns, return_value='')
+        args = '127.0.0.1'
+        mock(args)
+        mock.assert_called_once_with(args)
