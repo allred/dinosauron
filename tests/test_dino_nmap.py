ffd53e579d9793e19ae9bc63e9ac75597c3ecff2
Mike Allred <mikejallred@gmail.com>
2017-12-13 17:17:19 -0500

    Mock nmap.

--- tests/test_dino_nmap.py
+++ tests/test_dino_nmap.py
@@ -7,12 +7,13 @@ class TestDinoNmap(unittest.TestCase):
         self.dn = dino_nmap.DinoNmap()
 
     def test_scan_list(self):
-        l = self.dn.scan_list(["localhost"])
-        self.assertIsInstance(l, list)
-        self.assertEqual(len(l), 1)
+        args = '192.168.0.*'
+        mock = create_autospec(self.dn.scan_list, return_value=[])
+        mock(args)
+        mock.assert_called_once_with(args)
 
     def test_do_scan(self):
-        mock_function = create_autospec(self.dn.do_scan, return_value=[])
         args = [["192.168.1.1"], "-sV"]
+        mock_function = create_autospec(self.dn.do_scan, return_value=[])
         mock_function(*args)
         mock_function.assert_called_once_with(*args)
